7966fb36f96c47c2485faee7418a0a4297902d9c
KaloRem <bogdan.paslawskii@gmail.com>
2023-12-03 16:37:10 +0100

    v1.2

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
 
@@ -15,11 +7,4 @@ export const store = configureStore({
     contacts: contactsReducer,
     filter: filterReducer,
   },
-
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
